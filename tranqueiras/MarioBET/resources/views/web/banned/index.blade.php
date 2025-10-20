865981f2e103480c1737700a4cec2cf9d1ec86c4
gregorio <gregorioponci.91@gmail.com>
2025-10-19 22:38:08 -0300

    mario bet

---
+++ tranqueiras/MarioBET/resources/views/web/banned/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.web')
+
+@push('styles')
+
+@endpush
+
+@section('content')
+    <div class="container">
+        @include('includes.navbar_left')
+
+    </div>
+@endsection
+
+@push('scripts')
+
+@endpush
